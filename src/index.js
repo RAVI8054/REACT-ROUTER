e3e0a65780dbfbd2b9aed107cec977cc5f628bf0
RAVI8054 <ravi.jsd@hotmail.com>
2024-10-22 14:36:44 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './componets/Home/Home';
+import About from './componets/About/About';
+import Contact from './componets/Contact/Contact';
+// import User from './componets/User/User';
+import Github from './componets/Github/Github';
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "About",
+//         element: <About />,
+//       },
+//       {
+//         path: "Contact",
+//         element: <Contact/>,
+//       }
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="About" element={<About />} />
+      <Route path="Contact" element={<Contact />} />
+      {/* <Route path="User/:Userid" element={<User />} /> */}
+      <Route path="Github" element={<Github />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
